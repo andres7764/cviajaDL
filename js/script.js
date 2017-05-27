e0f4926b86f6c28cc157a9915ebd6257cbe88058
franklynsistemas <franking.sistemas@gmail.com>
2017-05-26 22:33:46 -0500

    Mejoras

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
 
-	$("#target").submit(function(event){
-		/*if(){
+	var campos = ["nombre","correo"];
 
-		} else {
+	function validFields(){
+		for(i in campos){
+			if($("#"+campos[i]).val() === ""){
+				return false;
+			}
+		}
+		return true;
+	}
 
-		}*/
-	var sPagos 
-	var f = new Date();
-		$.	ajax({ 
+	function sendPost(typeSubcri){
+		var tmpOtro = ($("#avarias:checked").length > 0)? true +$("#avarias").val()  : false+$("#avarias").val();
+		var f = new Date();
+		$.ajax({ 
 		  url: "http://serverC.boxo.com.co/saveContact",
 		  method:"POST",
 		  data: {
@@ -18,7 +24,45 @@ $(document).ready(function(){
 		  		caminata: ($("#caminata:checked").length > 0)? true : false,
 		  		picknic: ($("#Picknic:checked").length > 0)? true : false,
 		  		sturisticos: ($("#sturisticos:checked").length > 0)? true : false,
-		  		otro: ($("#avarias:checked").length > 0)? true : false
+		  		otro: tmpOtro
+		  	},
+		  	suscribirseMail: ($("#sMail:checked").length > 0)? true : false,
+		  	susCribirsePagos:  typeSubcri,
+		  	fecha: f
+		  }})
+		.done(function(sucs) {
+			alert(sucs.token);
+			console.log(sucs);
+		})
+	};
+
+
+	$("#btnFree").click(function(){
+		validFields() ? sendPost(false) : alert("Debe ingresar su nombre y correo!");
+	});
+
+	$("#btnCost").click(function(){
+		validFields() ? sendPost(true) : alert("Debe ingresar su nombre y correo!");
+	});
+
+	/*
+		$("#target").submit(function(event){
+	var sPagos;
+
+	var tmpOtro = ($("#avarias:checked").length > 0)? true +$("#avarias").val()  : false+$("#avarias").val();
+
+	var f = new Date();
+		$.ajax({ 
+		  url: "http://serverC.boxo.com.co/saveContact",
+		  method:"POST",
+		  data: {
+		  	nombre: $("#nombre").val(),
+		  	correo: $("#correo").val(),
+		  	opciones: {
+		  		caminata: ($("#caminata:checked").length > 0)? true : false,
+		  		picknic: ($("#Picknic:checked").length > 0)? true : false,
+		  		sturisticos: ($("#sturisticos:checked").length > 0)? true : false,
+		  		otro: tmpOtro
 		  	},
 		  	suscribirseMail: ($("#sMail:checked").length > 0)? true : false,
 		  	susCribirsePagos:  sPagos,
@@ -29,5 +73,7 @@ $(document).ready(function(){
 				console.log(sucs);
 		})
 
-	})
+	});
+	*/
+
 })
