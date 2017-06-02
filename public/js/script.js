f244dee2bf1c1ff38af8855261b32804aa66e3a7
andres7764 <andres7764@hotmail.com>
2017-06-01 19:12:54 -0500

    Version 3
    Version 3

--- public/js/script.js
+++ public/js/script.js
@@ -12,6 +12,7 @@ $(document).ready(function(){
 	}
 
 	function sendPost(typeSubcri){
+		alert("si");
 		var tmpOtro = ($("#avarias:checked").length > 0)? true +$("#avarias").val()  : false+$("#avarias").val();
 		var f = new Date();
 		$.ajax({ 
