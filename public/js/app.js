bb61800f9a26012cf4c96d39872f55f7d05bf3d1
franklynsistemas <franking.sistemas@gmail.com>
2017-06-17 13:02:30 -0500

    Imprim activities dinamic and btnReserve

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@
       $routeProvider.
         when('/', {
           templateUrl: 'templates/home.html',
-          controller: 'activityCtrl'
+          controller: 'activitiesCtrl'
         })
         .when('/catalogo/:activity', {
           templateUrl: 'templates/activity.html',
@@ -18,8 +18,9 @@
         $scope.actividad = {};
         $scope.image = '';
         console.log($routeParams.activity);
+        var activity = $routeParams.activity ? $routeParams.activity : "59431cf6a2bf7c1f18aeee39";
         $http.defaults.headers.post["Content-Type"] = "application/json";
-        $http.get('/getActivity?id='+$routeParams.activity).then(function(result){
+        $http.get('/getActivity?id='+activity).then(function(result){
             $scope.activity = result.data.activity[0];
             $scope.image = $scope.activity.image;
             createMap($scope.activity.location);
@@ -27,12 +28,13 @@
         });
         
         function createMap(location){
+            var latLng  = {lat: location.lat, lng: location.lng};
             var map = new google.maps.Map(document.getElementById('map'), {
               zoom: 12,
-              center: location
+              center: latLng
             });
             var marker = new google.maps.Marker({
-              position: location,
+              position: latLng,
               map: map
             });
         };
@@ -43,23 +45,37 @@
         
         $scope.activities = {};
         
-        $scope.irA = function(){
-            $location.url('/catalogo/594315bb5c00d02b14f4fd96');
+        $scope.irA = function(id){
+            console.log(id);
+            $location.url('/catalogo/'+id);
         }
         $http.defaults.headers.post["Content-Type"] = "application/json";
         $http.get('/getActivities').then(function(result){
             $scope.activities = result.data.activities; 
         });
-	/*$timeout(function(){
-	  $http({
-			method: 'post',
-			url:'/getActivities',
-		}).then(function(response){
-		   $scope.activities = {};
-		   console.log(response.data);
-		   $scope.activities = response.data.data;
-		})
-	  },1000);*/
+            
+        var modal = document.getElementById('modalReservar');
+        // Get the button that opens the modal
+        var btn = document.getElementById("btnOpenModal");
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+        // When the user clicks on the button, open the modal 
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+        
 	});
     
     cviaja.directive('dynamicElement', ['$compile', function ($compile) {
