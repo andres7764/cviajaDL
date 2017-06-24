a03745b82726e0c9dd8172b16649541e4c0972cb
franklynsistemas <franking.sistemas@gmail.com>
2017-06-24 00:28:04 -0500

    hakaton first part

--- models/modelReservas.js
+++ models/modelReservas.js
@@ -2,12 +2,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 //User collection
 reservas = new mongoose.Schema({
-  nombre:             { type: String },
-  correo:             { type: String },
-  event: 			  {	type: Schema.ObjectId, ref: "activitiesmodel"},
-  fecha:              { type: Date, default: new Date() },
-  quantity: 		  { type: Number },
-  mount: 			  { type: Number }
+    nombre:     {type: String},
+    correo:     {type: String},
+    event: 		{type: Schema.ObjectId, ref: 'activitiesmodel'},
+    fecha:      {type: Date, default: new Date()},
+    quantity: 	{type: Number},
+    mount: 		   {type: Number},
+    typePayment:   {type: String},
+    wasPayment:    {type: Boolean, default: false}
 });
 
 module.exports = mongoose.model('reservasmodel',reservas);
