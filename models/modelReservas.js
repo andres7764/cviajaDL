b13d2f9a81be97d74a2e263ec73fe3b7350a8c76
andres7764 <andres7764@hotmail.com>
2017-06-27 10:24:58 -0500

    save

--- models/modelReservas.js
+++ models/modelReservas.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 //User collection
 reservas = new mongoose.Schema({
-
     nombre:     {type: String},
     correo:     {type: String},
     event: 		{type: Schema.ObjectId, ref: 'activitiesmodel'},
